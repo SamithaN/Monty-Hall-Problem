d85ddcbac75a694359903b50435d1d4a3de7d3e8
SamithaN <samithaneranjan23@gmail.com>
2024-08-07 10:35:23 +0530

    project initialized

---
+++ backend/MontyHall_be/models/GameState.cs
@@ -0,0 +1,12 @@
+﻿namespace MontyHall_be.models
+{
+    public class GameState
+    {
+        public int WinningDoor { get; set; }
+        public int UserChoice { get; set; }
+        public int MontyReveal { get; set; }
+        public bool GameOver { get; set; }
+        public bool? UserWon { get; set; }
+
+    }
+}
